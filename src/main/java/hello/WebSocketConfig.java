1ab2a7c03c9f30dcfdc6e2e1a403a17ca569bcab
xieyuxing <185246042@qq.com>
2018-11-12 17:57:43 +0800

    a

--- src/main/java/hello/WebSocketConfig.java
+++ src/main/java/hello/WebSocketConfig.java
@@ -12,10 +12,14 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
+//        config.enableSimpleBroker("/topic");
         config.setApplicationDestinationPrefixes("/app");//目标端点的前缀，即客户端的前缀
     }
 
+    /**
+     * 设置连接服务器时的端点，即客户端调用connect时的端点
+     * @param registry
+     */
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/gs-guide-websocket").setAllowedOrigins("*").withSockJS();
--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -13,9 +13,9 @@ function setConnected(connected) {
 }
 
 function connect() {
-    //说明：如果在不跨域访问，new SockJS('/gs-guide-websocket')即可，而跨域的话使用ws://{ip}:{port}/gs-guide-websocket访问
+    //说明：如果在不跨域访问，new SockJS('/gs-guide-websocket')即可，而跨域的话使用http(s)://{ip}:{port}/gs-guide-websocket访问
     // var socket = new SockJS('/gs-guide-websocket');
-    var socket = new SockJS('ws://192.168.1.122:8080/gs-guide-websocket');
+    var socket = new SockJS('/gs-guide-websocket');
     stompClient = Stomp.over(socket);
     stompClient.connect({}, function (frame) {
         setConnected(true);
